84e87442233434108077212e9cb4be1cafb73594
Colton <colton@Coltons-MacBook-Air.local>
2020-04-05 04:12:00 -0500

    Homepage and Portfolio page cleaned up

--- nunjucks.js
+++ nunjucks.js
@@ -2,16 +2,21 @@ const nunjucks = require("nunjucks");
 const fs = require("fs");
 
 fs.writeFile("public/index.html", nunjucks.render("resources/html/index.njk"), function(err, data) {
-  if (err) console.log(err);
-  console.log("Compiled the Nunjucks, captain.");
+    if (err) console.log(err);
+    console.log("Compiled the Nunjucks, captain.");
 });
 
 fs.writeFile("public/generic.html", nunjucks.render("resources/html/generic.njk"), function(err, data) {
     if (err) console.log(err);
     console.log("Compiled the Nunjucks, captain.");
-  });
+});
 
-  fs.writeFile("public/elements.html", nunjucks.render("resources/html/elements.njk"), function(err, data) {
+fs.writeFile("public/elements.html", nunjucks.render("resources/html/elements.njk"), function(err, data) {
     if (err) console.log(err);
     console.log("Compiled the Nunjucks, captain.");
-  });
+});
+
+fs.writeFile("public/portfolio.html", nunjucks.render("resources/html/portfolio.njk"), function(err, data) {
+    if (err) console.log(err);
+    console.log("Compiled the Nunjucks, captain.");
+});
